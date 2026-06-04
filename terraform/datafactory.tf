5c0fe81257e8b7b3362d8dc645cabe8094556a62
Carlos Diaz Hernandez <carlosdiazdata@gmail.com>
2026-06-04 12:20:15 -0600

    fix(adf): rename HttpServer LS to LS_USGS_HTTP_V2 to avoid state conflict
    Old Web linked service (LS_USGS_HTTP) still exists in ADF and is
    referenced by the dataset. Using a different name allows Terraform
    to create the new HttpServer LS, update the dataset, then destroy
    the orphaned Web LS in a single apply.

--- terraform/datafactory.tf
+++ terraform/datafactory.tf
@@ -38,7 +38,7 @@ resource "azurerm_role_assignment" "adf_to_databricks" {
 # ---------------------------------------------------------------------------
 
 resource "azurerm_data_factory_linked_custom_service" "usgs" {
-  name                 = "LS_USGS_HTTP"
+  name                 = "LS_USGS_HTTP_V2"
   data_factory_id      = azurerm_data_factory.main.id
   type                 = "HttpServer"
   type_properties_json = jsonencode({
